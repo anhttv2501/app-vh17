81496dc938927c63f9b2b3231f21c888f023c4e3
anhttv2501 <anhttv2501@gmail.com>
2026-03-16 11:21:26 +0700

    update App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ function App() {
   return (
     <>
     <h1>Lớp VH17!</h1>
+    <table border={1}>
+      <tr>
+        <th>STT</th>
+        <th>Ma sinh vien</th>
+        <th>Ho va ten</th>
+        <th>Diem</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>B23DVCN005</td>
+        <td>Tô an An</td>
+        <td>5</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>B23DVCN876</td>
+        <td>Vũ Thị Lan</td>
+        <td>7</td>
+      </tr>
+    </table>
     </>
   );
 }
